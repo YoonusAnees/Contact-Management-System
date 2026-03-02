75662575f7f6e646d0e26698527e571479b882f4
yoonusanees <yoonusaneesniis@gmail.com>
2026-03-02 17:26:29 +0530

    feat(db added)

---
+++ db/db.php
@@ -0,0 +1,23 @@
+<?php
+// config/db.php
+
+declare(strict_types=1);
+
+$DB_HOST = "127.0.0.1";
+$DB_NAME = "veloz_contact";
+$DB_USER = "root";
+$DB_PASS = "";
+
+try {
+    $pdo = new PDO(
+        "mysql:host={$DB_HOST};dbname={$DB_NAME};charset=utf8mb4",
+        $DB_USER,
+        $DB_PASS,
+        [
+            PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+            PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+        ]
+    );
+} catch (Throwable $e) {
+    die("Database connection failed: " . $e->getMessage());
+}
